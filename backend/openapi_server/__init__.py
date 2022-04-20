35f7f8154cd540ac0ccda8445897178df7ef272d
wenhaohaoo <lwen.hao93@gmail.com>
2022-04-20 20:11:05 +0800

    corsify response

--- backend/openapi_server/__init__.py
+++ backend/openapi_server/__init__.py
@@ -7,11 +7,18 @@ from openapi_server import encoder
 
 db = MongoClient(Config.MONGODB_CONNECT, username=Config.MONGODB_USERNAME, password=Config.MONGODB_PASSWORD).tweets
 
+def set_cors_headers_on_response(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'X-Requested-With'
+    response.headers['Access-Control-Allow-Methods'] = 'OPTIONS'
+    return response
+
 def create_app(config_class=Config):
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml',
                 arguments={'title': 'COVID-19 Tweets Sentiment Analysis API'},
                 pythonic_params=True)
+    app.app.after_request(set_cors_headers_on_response)
 
     return app
